e578adaaa54ba037d589e3fa2b032a893dd87b66
Kendall Hudson <64994077+kendallhudson@users.noreply.github.com>
2023-01-05 19:58:45 -0600

    added latest updates

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const morgan = require('morgan');
+const postBank = require('./postBank');
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+}
+
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
+// RaveBlue
