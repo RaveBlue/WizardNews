ff6fdd7230fd43e02c51d674cdc14aadba154ab4
Kendall Hudson <64994077+kendallhudson@users.noreply.github.com>
2023-01-08 18:58:48 -0600

    completed Add a Single-Post Route part

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(morgan("dev"));
 
 app.use(express.static("public"));
 
+// STYLING THE INITIAL ROUTE
+
 app.get("/", (req, res) => {
   const posts = postBank.list();
 
@@ -42,10 +44,41 @@ app.get("/", (req, res) => {
   res.send(html);
 });
 
+// ADD A SINGLE POST ROUTE
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+
+  const post = postBank.find(id);
+
+  const htmlId = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            <span class="news-position"></span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+            <p>${post.content}</p>
+          </p>
+        </div>
+    </div>
+  </body>
+</html>`;
+
+res.send(htmlId)
+});
+
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
 
-// RaveBlue
+
